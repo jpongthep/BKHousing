f2ed66f098085274e02db917245c1ce46330002b
Pongthep Janseana <pongthep@rtaf.mi.th>
2021-09-02 10:41:34 +0700

    640902:1041 Add allows host

--- Housing/production.py
+++ Housing/production.py
@@ -1,12 +1,13 @@
 from .settings import *
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'ec2-54-151-164-105.ap-southeast-1.compute.amazonaws.com']
 DEBUG = False
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'atkqueuedb',
-        'username' : armis_user
+        'username' : 'armis_user'
         'PASSWORD' : '',
         'HOST' : 'localhost',
         'PORT' : '3306'  ,          
