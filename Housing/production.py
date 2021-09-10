07853dcfeddbf6e3cf2aa1062824cbfaec9835cb
jpongthep <38453287+jpongthep@users.noreply.github.com>
2021-09-10 11:12:01 +0700

    Update production.py

--- Housing/production.py
+++ Housing/production.py
@@ -1,18 +1,1 @@
-from .settings import *
 
-DEBUG = False
-ALLOWED_HOSTS = ['*','ec2-54-151-164-105.ap-southeast-1.compute.amazonaws.com']
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'armis_db',
-	    'HOST' : 'aws-mysqlhunterdogdb.cevi5wuglaux.ap-southeast-1.rds.amazonaws.com',
-        'USER' : 'armis_user',        
-        'PASSWORD' : 'YD$^55#r@deN',
-        'PORT' : '3306' ,
-        'OPTIONS': {
-            "init_command": "SET sql_mode='STRICT_TRANS_TABLES'",
-        }
-    }
-}
